2825d4abd81e064ecfd22ac4a13eb1da9560416d
LeChat <sedoykin@gmail.com>
2025-08-03 21:51:42 +0300

    Initial Commit

---
+++ TCSwiftClientDebug/TCSwiftClientDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCSwiftClientDebugApp.swift
+//  TCSwiftClientDebug
+//
+//  Created by Sedoykin Alexey on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCSwiftClientDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
